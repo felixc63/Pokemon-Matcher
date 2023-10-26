ece8664f798dbc88390f6518c6f6442978056539
felixc63 <felixc63@nycstudents.net>
2023-10-26 15:33:56 -0400

    commit

--- matchgamescript.js
+++ matchgamescript.js
@@ -1,5 +1,11 @@
+const cards = document.getElementsByClassName("cards");
+const front = document.getElementsByClassName("front");
+const back = document.getElementsByClassName("back");
 const frontCards = document.getElementsByClassName("frontCards");
+const backCards = document.getElementsByClassName("backCards");
 
-for(let i = 0; i < frontCards.length; i++){
-    
+for(let i = 0; i < cards.length; i++){
+    cards[i].onclick = function(){
+        cards[i].classList.toggle("flip");
+    }
 }
